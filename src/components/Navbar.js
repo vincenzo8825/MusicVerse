c3f9afef4cb7edfa4eb07df4ecc8d0d180112ec9
vincenzo8825 <vincenzorocca88@gmail.com>
2024-05-02 17:46:17 +0200

    terzo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import'./../css/navbar.css';
 function Navbar() {
   return (
     <nav>
+<h2>Navbar</h2>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/artist">Artist</Link></li>
+        <li><Link to="/music">Music</Link></li>
         {/* Aggiungi altri link qui per le altre pagine del sito */}
       </ul>
     </nav>
-  );
+    
+);
 }
 
 export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,16 +12,48 @@ function Home() {
       <Navbar />
 
       {/* Contenuto della home */}
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <h1>Benvenuto nella nostra homepage!</h1>
-            <p>Qui troverai le ultime novità e le informazioni più importanti sul nostro sito.</p>
-            <button className="btn btn-primary">Pulsante Bootstrap</button>
+      <section className="hero-section">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <h1>Benvenuto su MusicVerse!</h1>
+              <p>Scopri la tua passione per la musica esplorando il vasto mondo dei generi, artisti e canzoni. Con MusicVerse, puoi trovare la tua musica preferita, leggere le ultime notizie e scoprire nuovi artisti.</p>
+              <a href="/about" className="btn btn-primary">Scopri di più</a>
+            </div>
+            <div className="col-lg-6">
+              <img src="/images/home-image.jpg" alt="Immagine di benvenuto" className="img-fluid" />
+            </div>
           </div>
-          
         </div>
-      </div>
+      </section>
+
+      {/* Featured artists section */}
+      <section className="featured-artists-section">
+        <div className="container">
+          <h2 className="section-title">Artisti in primo piano</h2>
+          <div className="row">
+            {/* Elenca qui alcuni artisti in primo piano */}
+            <div className="col-md-4">
+              <div className="card">
+                <img src="/images/artist1.jpg" className="card-img-top" alt="Artista 1" />
+                <div className="card-body">
+                  <h5 className="card-title">Artista 1</h5>
+                  <p className="card-text">Descrizione breve dell'artista.</p>
+                </div>
+              </div>
+            </div>
+            {/* Aggiungi più card degli artisti qui */}
+          </div>
+        </div>
+      </section>
+
+      {/* Call to action section */}
+      <section className="cta-section">
+        <div className="container">
+          <h2>Unisciti a noi e scopri il mondo della musica!</h2>
+          <a href="/signup" className="btn btn-lg btn-primary">Iscriviti ora</a>
+        </div>
+      </section>
 
       {/* Footer */}
       <Footer />
